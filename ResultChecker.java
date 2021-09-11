ba709439fd0955e28e559ccb1423c5e356e827f8
Suman manna <mannasuman611@gmail.com>
2021-09-11 15:49:05 +0530

    onActivityResult Checker

---
+++ ResultChecker.java
@@ -0,0 +1,19 @@
+package com.rward.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class ResultChecker {
+    static void setResult(Intent data){
+        //usage ResultChecker.setResult({Pass the Intent data});
+        Bundle bundle = data.getExtras();
+        if (bundle != null) {
+            for (String key : bundle.keySet()) {
+                Log.e("SUMAN", key + " : " + (bundle.get(key) != null ? bundle.get(key) : "NULL"));
+            }
+        } else {
+            Log.e("SUMAN", "No Data");
+        }
+    }
+}
