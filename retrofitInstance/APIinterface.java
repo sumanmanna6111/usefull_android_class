1830450e6d98a9efa18b9f5cac24e9c99903d825
Suman manna <mannasuman611@gmail.com>
2022-04-27 08:43:41 +0530

    Update APIinterface.java

--- retrofitInstance/APIinterface.java
+++ retrofitInstance/APIinterface.java
@@ -21,6 +21,7 @@ import retrofit2.http.Url;
 public interface APIinterface {
     /*@GET("api/rechist.php")
     Call<Post> getPosts();*/
+    
    @FormUrlEncoded
    @POST
    Call<Post> getPosts(@Url String url, @FieldMap Map<String,String> param);
@@ -32,16 +33,5 @@ public interface APIinterface {
     @GET("https://digitalcatalog.paytm.com/dcat/v1/browseplans/mobile/7166?channel=web&version=2&child_site_id=1&site_id=1&locale=en-in&pageCount=1&itemsPerPage=20&sort_price=asce&pagination=1")
     Call <ResponseBody> getResponse(@Query("operator") String operator, @Query("circle") String circle);
 
-    @FormUrlEncoded
-    @POST("https://webtoolscf.000webhostapp.com/apps/paytm/generateChecksum.php")
-    Call<Checksum> getChecksum(
-            @Field("MID") String mId,
-            @Field("ORDER_ID") String orderId,
-            @Field("CUST_ID") String custId,
-            @Field("CHANNEL_ID") String channelId,
-            @Field("TXN_AMOUNT") String txnAmount,
-            @Field("WEBSITE") String website,
-            @Field("CALLBACK_URL") String callbackUrl,
-            @Field("INDUSTRY_TYPE_ID") String industryTypeId
-    );
+  
 }
