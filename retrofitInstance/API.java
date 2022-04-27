eeb484f3cb07965397b4b4006623efeae51dad21
Suman manna <mannasuman611@gmail.com>
2022-04-27 08:41:12 +0530

    Update API.java

--- retrofitInstance/API.java
+++ retrofitInstance/API.java
@@ -1,9 +1,9 @@
 package com.rward.recharge.client;
 
 public class API {
-    static {
-        System.loadLibrary("recharge");
-    }
+//     static {
+//         System.loadLibrary("recharge");
+//     }
     public static String BASE_URL = URL();
     public static String login =  "login";
     public static String signup = "signup";
@@ -16,12 +16,12 @@ public class API {
     public static String dashboard = "dashboard";
     public static String rewards = "rewards";
     public static String rewardcollect = "rewardcollect";
-    public static String k1 = key1();
-    public static String k2 = key2();
-    public static String k3 = key3();
+//     public static String k1 = key1();
+//     public static String k2 = key2();
+//     public static String k3 = key3();
 
-    private static native String URL();
-    private static native String key1();
-    private static native String key2();
-    private static native String key3();
+//     private static native String URL();
+//     private static native String key1();
+//     private static native String key2();
+//     private static native String key3();
 }
