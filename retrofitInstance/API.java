77ace5e5d850f4c0102d5626791bcb7aa7b3600e
Suman manna <mannasuman611@gmail.com>
2022-04-27 08:41:33 +0530

    Update API.java

--- retrofitInstance/API.java
+++ retrofitInstance/API.java
@@ -7,15 +7,6 @@ public class API {
     public static String BASE_URL = URL();
     public static String login =  "login";
     public static String signup = "signup";
-    public static String transaction = "transaction";
-    public static String checktxnstatus = "txnstatus";
-    public static String addmoney = "addmoney";
-    public static String wallet = "wallet";
-    public static String recharge = "recharge";
-    public static String operator = "operator";
-    public static String dashboard = "dashboard";
-    public static String rewards = "rewards";
-    public static String rewardcollect = "rewardcollect";
 //     public static String k1 = key1();
 //     public static String k2 = key2();
 //     public static String k3 = key3();
