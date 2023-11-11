807da3c4da137e31dd7d4e3545d582eb930e916b
Suman manna <mannasuman611@gmail.com>
2023-11-11 11:08:01 +0530

    Delete retrofitInstance/API.kt

--- retrofitInstance/API.kt
+++
@@ -1,7 +0,0 @@
-package com.gtech.testnavgraph.network
-
-object API {
-    const val BASE_URL = "http://3.6.128.188/"
-
-    const val login = "send-message?token=c16eb960-456d-4ad4-85e4-13db795a423d"
-}
